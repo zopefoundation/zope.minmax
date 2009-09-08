2d59c506cf388b09f1d8c2a418d4238cb4caea12
Dan Korostelev <nadako@gmail.com>
2009-09-08 16:02:00 +0000

    Change mailing list address to zope-dev@zope.org, remove useless buildout part.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 setup(name='zope.minmax',
       version = '1.2.0dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description=("Homogeneous values favoring maximum or minimum for ZODB "
                    "conflict resolution"),
       long_description=(
