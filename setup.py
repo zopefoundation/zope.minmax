071f4df73114da500b27154f1b8f3e91f668a081
Zvezdan Petkovic <zpetkovic@acm.org>
2009-09-09 12:57:43 +0000

    Cleanup.
    - consistent indentation,
    - sort list entries,
    - one entry per line for easy addition/removal
    - prepare for a minor minor release to fix a PyPI page (link/email)

--- setup.py
+++ setup.py
@@ -20,44 +20,51 @@ def read(*rnames):
     text = open(os.path.join(os.path.dirname(__file__), *rnames)).read()
     return text
 
-setup(name='zope.minmax',
-      version = '1.2.0dev',
-      author='Zope Corporation and Contributors',
-      author_email='zope-dev@zope.org',
-      description=("Homogeneous values favoring maximum or minimum for ZODB "
-                   "conflict resolution"),
-      long_description=(
-          read('README.txt')
-          + '\n\n' +
-          'Detailed Documentation\n' +
-          '----------------------'
-          + '\n\n' +
-          read('src', 'zope', 'minmax', 'minmax.txt')
-          + '\n\n' +
-          read('CHANGES.txt')
-          ),
-      license='ZPL 2.1',
-      keywords=('zope3 zope zodb'),
-      classifiers = [
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope3'],
-      url='http://pypi.python.org/pypi/zope.minmax/',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['zope',],
-      extras_require=dict(
-          test=['zope.testing']),
-      install_requires=['setuptools',
-                        'zope.interface',
-                        'ZODB3',
-                        ],
-      include_package_data = True,
-      zip_safe = False,
-      )
+setup(
+    name='zope.minmax',
+    version='1.2.0dev',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description=(
+        "Homogeneous values favoring maximum or minimum for ZODB "
+        "conflict resolution"
+        ),
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        'Detailed Documentation\n' +
+        '----------------------'
+        + '\n\n' +
+        read('src', 'zope', 'minmax', 'minmax.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords=('zope3 zope zodb minimum maximum conflict resolution'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    url='http://pypi.python.org/pypi/zope.minmax/',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope',],
+    extras_require=dict(
+        test=['zope.testing']
+        ),
+    install_requires=[
+        'setuptools',
+        'ZODB3',
+        'zope.interface',
+        ],
+    include_package_data=True,
+    zip_safe=False,
+    )
