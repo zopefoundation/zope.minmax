eddecdbec242219bf1e3362b4a6b4901df27b608
Stephan Richter <stephan.richter@gmail.com>
2013-02-20 04:38:27 +0000

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name='zope.minmax',
-    version='2.0.0',
+    version='2.0.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description=(
