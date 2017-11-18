5711ad7918b42b66b790d0b9071e73fa826df20e
Jason Madden <jamadden@gmail.com>
2017-11-18 05:41:37 -0600

    https link

--- docs/conf.py
+++ docs/conf.py
@@ -270,7 +270,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'http://docs.python.org/': None,
+    'https://docs.python.org/': None,
     'https://persistent.readthedocs.io/en/latest/': None,
 }
 
