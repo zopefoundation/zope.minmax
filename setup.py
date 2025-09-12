883e14b95a2beee49ab9b64c6386c1f5f836b571
Michael Howitz <icemac@gmx.net>
2025-09-12 09:24:33 +0200

    Preparing release 4.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(
     name='zope.minmax',
-    version='4.0.dev0',
+    version='4.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description=(
