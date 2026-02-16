93fdaa4b003b88e15c62b765282d388a82575278
Michael Howitz <icemac@gmx.net>
2026-02-16 09:38:02 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -18,68 +18,8 @@
 ##############################################################################
 """Setup for zope.minmax package
 """
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-setup(
-    name='zope.minmax',
-    version='4.1.dev0',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    description=(
-        "Homogeneous values favoring maximum or minimum for ZODB "
-        "conflict resolution"
-    ),
-    long_description=(
-        read('README.rst')
-        + '\n\n' +
-        read('CHANGES.rst')
-    ),
-    license='ZPL-2.1',
-    keywords=('zope3 zope zodb minimum maximum conflict resolution'),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Zope :: 3',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    url='https://zopeminmax.readthedocs.io',
-    python_requires='>=3.10',
-    extras_require={
-        'test': [
-            'zope.testrunner >= 6.4',
-        ],
-        'docs': [
-            'Sphinx',
-            'repoze.sphinx.autointerface',
-        ],
-    },
-    install_requires=[
-        'persistent',
-        'setuptools',
-        'zope.interface',
-    ],
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
